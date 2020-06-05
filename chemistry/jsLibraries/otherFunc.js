a82e59fe944b4d39b51adb729b0f766ffc67ccf4
Gekocaretaker <61918937+Gekocaretaker@users.noreply.github.com>
2020-06-04 18:11:37 -0600

    Chemistry Mix rewrite
    Removed the need for external html.
    Add in mix functionality will be next.

--- chemistry/jsLibraries/otherFunc.js
+++ chemistry/jsLibraries/otherFunc.js
@@ -20,12 +20,23 @@ function oneTake(ele) {
 }
 
 //Create Elements
-function createEle(location, ident, imige) {
+function createEle(location, ident, boxSpot, imige) {
   var createDiv = document.createElement("div");
   createDiv.id = ident;
   createDiv.style.width = "28px";
   createDiv.style.height = "28px";
   $(location).appendChild(createDiv);
+  if(boxSpot == 1) {
+    $(ident).onclick = switchElems1;
+  } else if(boxSpot == 2) {
+    $(ident).onclick = switchElems2;
+  } else if(boxSpot == 3) {
+    $(ident).onclick = switchElems3;
+  } else if(boxSpot == 4) {
+    $(ident).onclick = switchElems4;
+  } else {
+    alert("Element in " + location + " is too high of a boxSpot value! Max value is 4!");
+  }
   var createImg = document.createElement("img");
   createImg.src = imige;
   createImg.style.width = "28px";
@@ -38,4 +49,29 @@ function createBre(location, ident) {
   createBreak.id = ident;
   createBreak.style.clear = "both";
   $(location).appendChild(createBreak);
+}
+
+//Switch Elements
+function switchElems1() {
+  $("greekSelect1").firstElementChild.src = this.querySelector("img").src;
+  $("gElements1").style.display = "none";
+  gEleOp1 = false;
+}
+
+function switchElems2() {
+  $("greekSelect2").firstElementChild.src = this.querySelector("img").src;
+  $("gElements2").style.display = "none";
+  gEleOp1 = false;
+}
+
+function switchElems3() {
+  $("americanSelect1").firstElementChild.src = this.querySelector("img").src;
+  $("aElements1").style.display = "none";
+  gEleOp1 = false;
+}
+
+function switchElems4() {
+  $("americanSelect2").firstElementChild.src = this.querySelector("img").src;
+  $("aElements2").style.display = "none";
+  gEleOp1 = false;
 }
