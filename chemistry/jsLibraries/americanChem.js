ccf3cf0c87c946adc7f091856945cc2ff5de79ca
Gekocaretaker <61918937+Gekocaretaker@users.noreply.github.com>
2020-06-05 21:28:07 -0600

    Added Elements
    Added final (mabe?) american elements.
    Next - replace the american element images. Those are placeholders until new graphics can be made.

--- chemistry/jsLibraries/americanChem.js
+++ chemistry/jsLibraries/americanChem.js
@@ -2,7 +2,14 @@
 var americanElements = ['Blank', 'hydrogen', 'helium', 'lithium', 'beryllium', 'boron', 'carbon', 'nitrogen', 'oxygen', 'fluorine',
   'neon', 'sodium', 'magnesium', 'aluminium', 'silocon', 'phosphorus', 'sulfur', 'chlorine', 'argon', 'potassium', 'calcium', 'scandium',
   'titanium', 'vanadium', 'chromium', 'manganese', 'iron', 'cobalt', 'nickel', 'copper', 'zinc', 'gallium', 'germanium', 'arsenic',
-  'selenium', 'bromine', 'krypton'];
+  'selenium', 'bromine', 'krypton', 'rubidium', 'strontium', 'yttrium', 'zirconium', 'niobium', 'molybdenium', 'technetium', 'ruthenium',
+  'rhodium', 'palladium', 'silver', 'cadmium', 'indium', 'tin', 'antimony', 'tellurium', 'iodine', 'xenon', 'caesium', 'barium',
+  'lanthanium', 'cerium', 'praseodymium', 'neodymium', 'promethium', 'samarium', 'europium', 'gadolinium', 'terbium', 'dysprosium',
+  'holmium', 'erbium', 'thulium', 'ytterbium', 'lutetium', 'hafnium', 'tantalum', 'tungsten', 'rhenium', 'osmium', 'iridium', 'platinum',
+  'gold', 'mercury', 'thallium', 'lead', 'bismuth', 'polonium', 'astatine', 'radon', 'francium', 'radium', 'actinium', 'thorium',
+  'protactinium', 'uranium', 'neptunium', 'plutonium', 'americium', 'curium', 'berkelium', 'californium', 'einsteinium', 'fermium',
+  'mendelevium', 'nobelium', 'lawrencium', 'rutherfordium', 'dubnium', ' seaborgium', 'bohrium', 'hassium', 'meitnerium', 'darmstadtium',
+  'roentgenium', 'copernicium', 'nihonium', 'flerovium', 'moscovium', 'livermorium', 'tennessine', 'oganesson'];
 var aEPhrase = ["It is useless.", "The first element on the table", "The lightest; Can't become solid", "Used to make batteries."];
 var americanEarned = 0;
 
